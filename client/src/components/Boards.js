5cda8c3b4154bd7f9ee4b1111f29b70703f3c020
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-24 12:09:10 +0000

    styled board componet

--- client/src/components/Boards.js
+++ client/src/components/Boards.js
@@ -1,1 +1,16 @@
-export default function Boards() {}
+import React from "react";
+import "../assets/boards.css";
+
+export default function Boards() {
+  return (
+    <section className="board">
+      <div className="board__name__container">
+        <h3 className="board__name">Board 1</h3>
+        <h3 className="board__name">Board 2</h3>
+        <h3 className="board__name">Board 3</h3>
+        <h3 className="board__name">Board 4</h3>
+      </div>
+      <button className="btn board__btn">Add Board</button>
+    </section>
+  );
+}
