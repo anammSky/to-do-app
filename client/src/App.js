6c57f6acc73f65540aad598e9e153fc0cced3cd5
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-24 11:00:32 +0000

    added main componets

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,15 @@
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Boards from "./components/Boards";
+import Footer from "./components/Footer";
+
 export default function App() {
-    return <h1>Hello, World!</h1>;
+  return (
+    <>
+      <Header />
+      <Boards />
+      <Main />
+      <Footer />
+    </>
+  );
 }
