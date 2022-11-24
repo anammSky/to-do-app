b1f480e6909c5f38a3a234514a8d096cd760ca3f
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-24 23:02:40 +0000

    added form handling states

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,10 +1,31 @@
 import "../assets/logins.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function LogIn() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  // do something about passwords
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    // function to api login
+  }
   return (
     <main className="login__main is--center-screen">
       <div className="login__container">
-        <form className="login__form">
+        <form onSubmit={handleSubmit} className="login__form">
           <h1 className="login__title">Log In</h1>
           <section className="login__section">
             <label className="login__label" htmlFor="username">
@@ -15,6 +36,8 @@ export default function LogIn() {
               type="text"
               id="username"
               name="username"
+              value={formData.username}
+              onChange={handleChange}
             ></input>
           </section>
           <section className="login__section">
@@ -26,14 +49,19 @@ export default function LogIn() {
               type="password"
               id="password"
               name="password"
+              value={formData.password}
+              onChange={handleChange}
             ></input>
           </section>
           <section className="login__section__btn">
-            <button className="btn login__btn" id="btnLogIn">
+            <button type="submit" className="btn login__btn" id="btnLogIn">
               Log In
+              {/* <Link to="/">Log In</Link>
+              Log in Validation
+            */}
             </button>
-            <button className="btn login__btn" id="btnSignUp">
-              Sign Up
+            <button type="button" className="btn login__btn" id="btnSignUp">
+              <Link to="/signup">Sign Up</Link>
             </button>
           </section>
         </form>
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,10 +1,36 @@
 import "../assets/logins.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-export default function LogIn() {
+export default function SignUp() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    repeatPassword: "",
+  });
+  // do something about passwords
+  // validate paswords match
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    // function to api login
+  }
   return (
     <main className="login__main is--center-screen">
       <div className="login__container">
-        <form className="login__form">
+        <form className="login__form" onSubmit={handleSubmit}>
           <h1 className="login__title">Sign Up</h1>
           <section className="login__section">
             <label className="login__label" htmlFor="username">
@@ -15,6 +41,8 @@ export default function LogIn() {
               type="text"
               id="username"
               name="username"
+              value={formData.username}
+              onChange={handleChange}
             ></input>
           </section>
           <section className="login__section">
@@ -26,6 +54,8 @@ export default function LogIn() {
               type="email"
               id="email"
               name="email"
+              value={formData.email}
+              onChange={handleChange}
             ></input>
           </section>
           <section className="login__section">
@@ -37,22 +67,33 @@ export default function LogIn() {
               type="password"
               id="password"
               name="password"
+              value={formData.password}
+              onChange={handleChange}
             ></input>
           </section>
           <section className="login__section">
-            <label className="login__label" htmlFor="repeat-password">
+            <label className="login__label" htmlFor="repeatPassword">
               Repeat Password
             </label>
             <input
               className="login__input"
               type="password"
-              id="repeat-password"
-              name="repeat-password"
+              id="repeatPassword"
+              name="repeatPassword"
+              value={formData.repeatPassword}
+              onChange={handleChange}
             ></input>
           </section>
           <section className="login__section__btn">
-            <button className="btn login__btn">Sign Up</button>
-            <button className="btn btn--danger login__btn">Cancel</button>
+            <button type="submit" className="btn login__btn">
+              {/* <Link to="/">Sign Up</Link>
+              Sign up and log in validation
+            */}
+              Sign Up
+            </button>
+            <button type="button" className="btn btn--danger login__btn">
+              <Link to="/login">Cancel</Link>
+            </button>
           </section>
         </form>
       </div>
