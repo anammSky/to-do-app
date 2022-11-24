30a39a4165b21e5d093ccee2a04007ee64f4d433
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-24 18:52:51 +0000

    added board input

--- client/src/components/Boards.js
+++ client/src/components/Boards.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/boards.css";
 
 export default function Boards() {
+  const [isShown, setIsShown] = useState(false);
+  function toggleShown() {
+    setIsShown((prevShown) => !prevShown);
+  }
   return (
     <section className="board">
       <div className="board__name__container">
@@ -9,8 +13,30 @@ export default function Boards() {
         <h3 className="board__name">Board 2</h3>
         <h3 className="board__name">Board 3</h3>
         <h3 className="board__name">Board 4</h3>
+        {isShown && (
+          <input
+            className="board__name__input"
+            type="text"
+            id="add-board"
+            name="add-board"
+          ></input>
+        )}
       </div>
-      <button className="btn board__btn">Add Board</button>
+      {!isShown && (
+        <button className="btn board__btn" onClick={toggleShown}>
+          Add Board
+        </button>
+      )}
+      {isShown && (
+        <div className="board__btn__container">
+          <button className="btn board__btn" onClick={toggleShown}>
+            Add
+          </button>
+          <button className="btn board__btn btn--danger" onClick={toggleShown}>
+            Cancel
+          </button>
+        </div>
+      )}
     </section>
   );
 }
