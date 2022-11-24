3fa0c95f66d4235bab1448204969b35e30d4a9bf
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-24 16:41:37 +0000

    added fetch request to main

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,53 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Task from "./Task";
-
-const sampleNotes = [
-  {
-    title: "title 1",
-    content:
-      "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
-  },
-  {
-    title: "title 2",
-    content:
-      "blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
-  },
-  { title: "title 3", content: "blah blah blah blah blah blah blah" },
-  {
-    title: "title 4",
-    content:
-      "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
-  },
-  { title: "title 5", content: "blah blah blah blah blah blah blah" },
-  {
-    title: "title 6",
-    content:
-      "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
-  },
-  { title: "title 7", content: "blah blah blah blah blah blah blah" },
-  { title: "title 8", content: "blah blah blah blah blah blah blah" },
-];
+import { useState, useEffect } from "react";
+import fetchGetAllTasks from "./utils/fetGetAllTasks";
 
 const Main = (props) => {
+  const [taskInfo, setTaskInfo] = useState({
+    title: "This is your first Task",
+    content: "What would you like to do?",
+  });
+  useEffect(() => {
+    let mounted = true;
+
+    fetchGetAllTasks().then((data) => {
+      if (mounted) {
+        setTaskInfo(data.result);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
+  console.log(taskInfo);
   const columnWrapper = {};
   const result = [];
-
   // create columns
   for (let i = 0; i < props.columns; i++) {
     columnWrapper[`column${i}`] = [];
   }
-
   // divide children into columns
-  for (let i = 0; i < sampleNotes.length; i++) {
+  for (let i = 0; i < taskInfo.length; i++) {
     const columnIndex = i % props.columns;
-    const { title, content } = sampleNotes[i];
+    const { title, content } = taskInfo[i];
 
     columnWrapper[`column${columnIndex}`].push(
       <Task key={i} margin={props.gap} title={title} content={content} />
     );
   }
-
   // wrap children in each column with a div
   for (let i = 0; i < props.columns; i++) {
     result.push(
