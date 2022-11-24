08afca8d25088fd6cf9391d7a1587eb93534ab11
Omar Amerah <omaramerah2@gmail.com>
2022-11-24 14:59:12 +0000

    Added item to test footer and tasks

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,1 +1,7 @@
-export default function Footer() {}
+export default function Footer() {
+    return(
+        <div>
+            <h1>Created by Ana, Arron, Scott and Omar!</h1>
+        </div>
+    )
+}
--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -1,15 +1,15 @@
-// import Main from "../components/Main";
+import Main from "../components/Main";
 import Header from "../components/Header";
 import Boards from "../components/Boards";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 
 export default function Tasks() {
   return (
     <>
       <Header />
       <Boards />
-      {/* <Main />
-      <Footer /> */}
+      <Main />
+      <Footer />
     </>
   );
 }
