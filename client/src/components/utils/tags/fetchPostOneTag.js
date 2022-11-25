4d80c7c8005f2c6fd5173dd238de7a6e3c33f1e2
Arron <arronmurray290104"gmail.com>
2022-11-25 14:54:46 +0000

    fixed the fetch for creating a tag

--- client/src/components/utils/tags/fetchPostOneTag.js
+++ client/src/components/utils/tags/fetchPostOneTag.js
@@ -3,19 +3,21 @@ export default async function fetchPostOneTag(tagTitle) {
         const response = await fetch("http://localhost:5001/tags", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: {
+            body: JSON.stringify({
                 title: tagTitle,
-            },
+            }),
         });
-        // if (response.status === 201) {
-        //     return data;
-        // } else {
-        //     if (data.errors !== undefined) {
-        //         alert(data.errors[0].msg);
-        //         return;
-        //     }
-        //     alert(data.message);
-        // }
+
+        const data = await response.json();
+        if (response.status === 201) {
+            return data;
+        } else {
+            if (data.errors !== undefined) {
+                alert(data.errors[0].msg);
+                return;
+            }
+            alert(data.message);
+        }
     } catch (error) {
         alert(error.message);
     }
