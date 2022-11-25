b53dacefecaf9947af36b504b76f8084c60905a1
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-25 11:41:33 +0000

    added input to edit task

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -7,7 +7,22 @@ import fetchDeleteOneTask from "./utils/tasks/fetchDeleteOneTask";
 export default function Task(props) {
   const id = props.id;
   const [isComplete, setIsComplete] = useState(false);
+  const [taskEdit, setTaskEdit] = useState({
+    title: props.title,
+    content: props.content,
+    isEdit: false,
+  });
+  // const [isEdit, setIsEdit] = useState(false);
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setTaskEdit((setTaskEdit) => {
+      return {
+        ...setTaskEdit,
+        [name]: value,
+      };
+    });
+  }
   function handleCheck(event) {
     const { checked } = event.target;
     // is this right???
@@ -15,8 +30,14 @@ export default function Task(props) {
     // function to api
     // add style to grey out task and out strokethrough
   }
+
   function handleEdit(event) {
+    setTaskEdit((prevTask) => {
+      return { ...prevTask, isEdit: !prevTask.isEdit };
+    });
     // call to api
+    // if button with id edit - change isEdit to true and change src/id/alt to save
+    // if button with id save - api path request - change isEdit to false, change svg src/alt/id to edit
     console.log(id);
   }
 
@@ -34,17 +55,29 @@ export default function Task(props) {
           value={isComplete}
           onChange={handleCheck}
         ></input>
-        <h1 className="task__title">{props.title}</h1>
+        {taskEdit.isEdit && (
+          <input value={taskEdit.title} name="title" onChange={handleChange} />
+        )}
+        {!taskEdit.isEdit && <h1 className="task__title">{taskEdit.title}</h1>}
       </div>
       <div className="task__section__content">
-        <p className="task__content">{props.content}</p>
+        {taskEdit.isEdit && (
+          <textarea
+            value={taskEdit.content ? taskEdit.content : " "}
+            name="content"
+            onChange={handleChange}
+          />
+        )}
+        {!taskEdit.isEdit && (
+          <p className="task__content">{taskEdit.content} </p>
+        )}
       </div>
       <div className="task__section__btns">
         <button type="button" onClick={handleDelete} className="task__btn">
           <img className="task__btn__img" src={deleteSVG} alt="delete" />
         </button>
         <button type="button" onClick={handleEdit} className="task__btn">
-          <img className="task__btn__img" src={edit} alt="edit" />
+          <img className="task__btn__img" src={edit} alt="edit" id="edit" />
         </button>
       </div>
     </article>
